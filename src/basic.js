cfb69207d459d749d6c1f73b36395f20c893fd17
Leander Seige <leander@seige.name>
2022-03-05 17:00:37 +0100

    update

---
+++ src/basic.js
@@ -0,0 +1,31 @@
+function toggleFullScreen() {
+  console.log("toggleFullScreen")
+  let e = document.getElementById("container")
+  console.log(e.fullscreenElement)
+  if (!document.fullscreenElement) {
+      document.getElementById("textarea").style.display="grid"
+      document.getElementById("shield").style.display="none"
+      e.requestFullscreen()
+  } else {
+    if (document.exitFullscreen) {
+      document.getElementById("textarea").style.display="none"
+      document.getElementById("shield").style.display="flex"
+      document.exitFullscreen()
+    }
+  }
+}
+
+function configurePlayer() {
+  console.log("configurePlayer")
+  console.log(window.screen.width)
+  if(window.screen.width<800 && !document.fullscreenElement) {
+    document.getElementById("textarea").style.display="none"
+    document.getElementById("shield").style.display="flex"
+  } else {
+    document.getElementById("textarea").style.display="grid"
+    document.getElementById("shield").style.display="none"
+  }
+}
+
+document.addEventListener("DOMContentLoaded", configurePlayer);
+window.addEventListener('resize', configurePlayer);
