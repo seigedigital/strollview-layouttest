3f20ad7de126112aa6c29843ef2489a88fcedda7
Leander Seige <leander@seige.name>
2022-03-11 08:18:40 +0100

    update

--- src/basic.js
+++ src/basic.js
@@ -1,5 +1,5 @@
 function isIOS() {
-  return [
+  let retval = [
     'iPad Simulator',
     'iPhone Simulator',
     'iPod Simulator',
@@ -8,6 +8,35 @@ function isIOS() {
     'iPod'
   ].includes(navigator.platform)
   || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  console.log("isIOS: "+retval)
+  return retval
+}
+
+function isTouchDevice() {
+  let retval = ( 'ontouchstart' in window ) ||
+    ( navigator.maxTouchPoints > 0 ) ||
+    ( navigator.msMaxTouchPoints > 0 );
+  console.log("isTouchDevice: "+retval)
+  return retval
+}
+
+function isFullScreen() {
+  let retval = (document.fullscreenElement||iOSFullScreenFlag)
+  console.log("isFullScreen: "+retval)
+  return retval
+}
+
+function isLandscape() {
+  let retval = window.screen.width>window.screen.height
+  console.log("isLandscape: "+retval)
+  return retval
+}
+
+function isSmallScreen() {
+  let calcWidth = window.screen.width<window.innerWidth?window.screen.width:window.innerWidth
+  let retval = calcWidth<800
+  console.log("isSmallScreen: "+retval)
+  return retval
 }
 
 function toggleFullScreen() {
@@ -66,49 +95,53 @@ function enableScrolling() {
 
 function toggleFullScreenIOS() {
   console.log("toggleFullScreenIOS "+iOSFullScreenFlag)
-  let e = document.getElementById("container")
   iOSFullScreenFlag=iOSFullScreenFlag==false?true:false
   if(iOSFullScreenFlag) {
     disableScrolling()
     configurePlayer()
-    e.classList.add("iosfullscreen")
   } else {
     enableScrolling()
     configurePlayer()
-    e.classList.remove("iosfullscreen")
   }
 }
 
 function configurePlayer() {
   console.log("configurePlayer")
-  document.getElementById("container").className="contained"
-  document.getElementById("textarea").className="contained_left"
   console.log("Screen width: "+window.screen.width)
-  if( window.screen.width<800 && !(document.fullscreenElement||iOSFullScreenFlag) ) {
-    // mobile mode
-    document.getElementById("textarea_content").style.display="none"
-    document.getElementById("textbelow").style.display="block"
-    document.getElementById("heading1").style.display="block"
-    document.getElementById("heading2").style.display="block"
-    // document.getElementById("shield").style.display="flex"
-    if(window.screen.width>window.screen.height) {
-      document.getElementById("textarea").className="contained_left"
-    } else {
-      document.getElementById("textarea").className="contained_superbottom"
-    }
+  console.log("Window width: "+window.innerWidth)
+
+  document.getElementById("container").className="contained"
+  if(iOSFullScreenFlag) {
+    document.getElementById("container").classList.add("iosfullscreen")
   } else {
+    document.getElementById("container").classList.remove("iosfullscreen")
+  }
+
+  if( ((!isTouchDevice()) && (!isSmallScreen())) || isFullScreen() ) {
     document.getElementById("textarea_content").style.display="grid"
     document.getElementById("textbelow").style.display="none"
     document.getElementById("heading1").style.display="none"
     document.getElementById("heading2").style.display="none"
-    // document.getElementById("shield").style.display="none"
-    if(window.screen.width>window.screen.height) {
+    if(isLandscape()) {
       document.getElementById("textarea").className="contained_left"
     } else {
       document.getElementById("textarea").className="contained_bottom"
     }
+  } else {
+    document.getElementById("textarea_content").style.display="none"
+    document.getElementById("textbelow").style.display="block"
+    document.getElementById("heading1").style.display="block"
+    document.getElementById("heading2").style.display="block"
+    document.getElementById("textarea").className="contained_superbottom"
   }
+
 }
 
-document.addEventListener("DOMContentLoaded", configurePlayer)
-window.addEventListener('resize', configurePlayer)
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOMLoaded event.")
+  configurePlayer()
+})
+window.addEventListener('resize', () => {
+  console.log("Resize event.")
+  configurePlayer()
+})
